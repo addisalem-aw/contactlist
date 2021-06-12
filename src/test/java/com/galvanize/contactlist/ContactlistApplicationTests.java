afa8dedb8df06f3ef77d14b5940fe4c98980ea0f
Addisalem Wordofa <addaber@gmail.com>
2021-06-12 12:12:44 -0400

    inital commit

---
+++ src/test/java/com/galvanize/contactlist/ContactlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.contactlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
