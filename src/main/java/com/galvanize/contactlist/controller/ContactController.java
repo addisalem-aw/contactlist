7a8b892b8ac90be1cb978f1b33a6396ee0773d2a
Addisalem Wordofa <addaber@gmail.com>
2021-06-12 18:33:04 -0400

    get by name and surname api updated

--- src/main/java/com/galvanize/contactlist/controller/ContactController.java
+++ src/main/java/com/galvanize/contactlist/controller/ContactController.java
@@ -24,18 +24,12 @@ public class ContactController {
     @GetMapping("/contacts")
     public ResponseEntity<List<Contacts>> listContactByGivenName(@RequestParam(required = false) String givenName,@RequestParam(required = false) String surname)
     {
-        if(givenName!=null)
+        if(givenName!=null && surname==null)
         return new ResponseEntity(contactService.findByGivenName(givenName), HttpStatus.OK);
         else if(givenName!=null && surname!=null)
             return new ResponseEntity(contactService.findByNames(givenName,surname), HttpStatus.OK);
         else
             return new ResponseEntity(contactService.findAllContacts(), HttpStatus.OK);
-
-    }
-    @GetMapping("/contacts/givenname")
-    public ResponseEntity<Contacts> listContactByNames(@PathVariable("givenName") String givenName, @PathVariable("surname") String surname)
-    {
-            return new ResponseEntity(contactService.findByNames(givenName,surname), HttpStatus.OK);
 
     }
 
--- src/main/java/com/galvanize/contactlist/repository/ContactRepository.java
+++ src/main/java/com/galvanize/contactlist/repository/ContactRepository.java
@@ -3,18 +3,15 @@ package com.galvanize.contactlist.repository;
 import com.galvanize.contactlist.domain.Contacts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contacts,Long> {
-//    @Query("select * from contacts c where c.givenName = givenName and c.surname=surname")
-    List<Contacts> findByGivenName(String givenName);
-//    @Query("select * from contacts c where c.givenName = 'test'")
-//    List<Contacts> findByNames(String givenName, String surname);
 
-    @Query("SELECT c FROM contacts c WHERE c.givenName LIKE %:givenName% and c.surname LIKE %:surname% ")
-    List<Contacts> findByNames(@Param("givenName") String givenName,@Param("surname") String surname );
+    List<Contacts> findByGivenName(String givenName);
+
+    @Query("SELECT c FROM Contacts c WHERE c.givenName = ?1 AND c.surname = ?2 ")
+    List<Contacts> findByNames(String givenName, String surname );
 }
