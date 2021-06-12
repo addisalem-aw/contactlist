28148bc40971f05398a88a0d7c6b379619f1392a
Addisalem Wordofa <addaber@gmail.com>
2021-06-12 12:46:44 -0400

    getAllContactList API-update

--- src/main/java/com/galvanize/contactlist/repository/ContactRepository.java
+++ src/main/java/com/galvanize/contactlist/repository/ContactRepository.java
@@ -2,6 +2,8 @@ package com.galvanize.contactlist.repository;
 
 import com.galvanize.contactlist.domain.Contacts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contacts,Long> {
 }
--- src/main/java/com/galvanize/contactlist/service/ContactService.java
+++ src/main/java/com/galvanize/contactlist/service/ContactService.java
@@ -3,9 +3,10 @@ package com.galvanize.contactlist.service;
 import com.galvanize.contactlist.domain.Contacts;
 import com.galvanize.contactlist.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class ContactService {
     @Autowired
     ContactRepository contactRepository;
